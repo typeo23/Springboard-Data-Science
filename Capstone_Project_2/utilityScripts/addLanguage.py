78bae6cfd138a190422f93071d77eb5a7abd246e
Itay Barel <ityabar@hotmail.com>
2018-05-26 22:52:55 -0700

    Adds a language column

---
+++ Capstone_Project_2/utilityScripts/addLanguage.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import sys
+import csv
+from polyglot.detect import Detector
+import re
+
+maxInt = sys.maxsize
+decrement = True
+
+while decrement:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+
+    decrement = False
+    try:
+        csv.field_size_limit(maxInt)
+    except OverflowError:
+        maxInt = int(maxInt/10)
+        decrement = True
+
+df = pd.read_csv(open('../data/csv/s2-corpus-0000.csv', 'rU'), engine='c')
+lang = []
+for i, abstract in enumerate(df.paperAbstract.values):
+    try:
+        lang.append(Detector(abstract, quiet=True).language.name)
+    except:
+        lang.append('unKnown')
+df['lang'] = lang
+df.to_csv('../data/csv/s2-corpus-00_lang.csv')

6e7cdcf450e494e73eec8686dcf1505ff2397cbf
Itay Barel <ityabar@hotmail.com>
2018-05-31 19:40:57 -0700

    minor changes for trining on the reduced dataset

--- Capstone_Project_2/src/train_doc2vec.py
+++ Capstone_Project_2/src/train_doc2vec.py
@@ -1,23 +1,25 @@
 import gensim
-from trainText.trainFolder import LabeledDir
+from trainText.trainFolder import LabeledDirCsvFiles
+import sys
 
 """
 A simple testing scrip
 Train for 40 epochs, the model parameters are largely the defaults
 """
+#sys.stderr = open('err.txt', 'w')
+assert gensim.models.doc2vec.FAST_VERSION > -1
 
-ld = LabeledDir('../data/', 'gz')
-epoches = 40
+ld = LabeledDirCsvFiles('../data/csvTrain/', 'csv')
+epoches = 200
 
+model = gensim.models.Doc2Vec(vector_size=400,  workers=6)
 
-model = gensim.models.Doc2Vec(vector_size=400, window=15, min_count=10,
-                              workers=20)
-
-print ('Building vocabulary.....')
+print('Building vocabulary.....')
 model.build_vocab(ld)
+model.save('../models/doc2vec/doc2vec_one_file_filter.model')
 
-print ('Training model.....')
+print('Training model.....')
 model.train(ld, total_examples=model.corpus_count, epochs=epoches)
 
-print ('Saving model.....')
-model.save('doc2vec_one_file.model')
+print('Saving model.....')
+model.save('../models/doc2vec/doc2vec_one_file_filter.model')
